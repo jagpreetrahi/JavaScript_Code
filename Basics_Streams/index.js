9b0aea2fcebdb85368e896388bfb8e97e9215eda
jagpreetrahi <jagpreet1615@gmail.com>
2024-08-02 11:14:20 +0530

    This is my commit

---
+++ Basics_Streams/index.js
@@ -0,0 +1,22 @@
+
+import { read, write } from 'fs';
+import {readFile , writeFile} from  'fs/promises';
+const filepath = new URL('./index.html' , import.meta.url);
+
+let readContent = await readFile(filepath , {encoding : 'utf-8'});
+
+console.log(readContent);
+
+const data = {
+    name : "jagpreet",
+    course : "BE",
+    age : '23'
+}
+
+for(const[key , value] of Object.entries(data)){
+    readContent = readContent.replace(`{${key}}` ,value)
+}
+
+// await writeFile(filepath  , readContent)
+await writeFile(new URL('./newIndex.html' , import.meta.url)  , readContent);
+
